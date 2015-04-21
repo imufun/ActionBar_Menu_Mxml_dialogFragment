3a529d311033fe5e603b80e326cee84864793145
imufun <imufun1@gmail.com>
2015-04-21 20:51:27 +0600

    Initial commit

---
+++ app/src/androidTest/java/com/imufun/imran/actionbar_menu_mxml_dialogfragment/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.imufun.imran.actionbar_menu_mxml_dialogfragment;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
